bd379527a6e3e1c83310926a32fa3388c6044ecc
MrAndreID <andreaadam215@gmail.com>
2021-01-25 16:40:59 +0700

    add func. for error message;

---
+++ gohelpers.go
@@ -0,0 +1,19 @@
+package gohelpers
+
+import (
+	"log"
+	"fmt"
+)
+
+func ErrorMessage(message string, err interface{}) {
+	fmt.Println()
+	log.Println("-------------------- Start Error Message --------------------")
+	log.Println("Message => " + message + ".")
+
+	if err != nil {
+		log.Println("Error => ", err)
+	}
+
+	log.Println("-------------------- End Of Error Message --------------------")
+	fmt.Println()
+}
