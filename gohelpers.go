a65a949f3cf7d61143ee3a12958a2408bac96dfa
MrAndreID <andreaadam215@gmail.com>
2021-01-25 18:50:31 +0700

    add json encode, bytes, random byte, random string, random int;

--- gohelpers.go
+++ gohelpers.go
@@ -1,8 +1,14 @@
 package gohelpers
 
 import (
-	"log"
 	"fmt"
+	"log"
+	"bytes"
+	"crypto/rand"
+	"encoding/json"
+	"encoding/base64"
+	"encoding/binary"
+	mathRand "math/rand"
 )
 
 func ErrorMessage(message string, err interface{}) {
@@ -16,4 +22,40 @@ func ErrorMessage(message string, err interface{}) {
 
 	log.Println("-------------------- End Of Error Message --------------------")
 	fmt.Println()
+}
+
+func JSONEncode(data interface{}) string {
+	jsonResult, _ := json.Marshal(data)
+
+	return string(jsonResult)
+}
+
+func Bytes(n int) []byte {
+	b := make([]byte, n)
+	_, _ = rand.Read(b)
+
+	return b
+}
+
+func RandomByte(n int) string {
+	return base64.URLEncoding.EncodeToString(Bytes(n))
+}
+
+func RandomString(length int) string {
+	var bytesBuffer bytes.Buffer
+
+	bytesBuffer.Grow(length)
+
+	letterRunes := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+	letterLength := uint32(len(letterRunes))
+
+	for i := 0; i < length; i++ {
+		bytesBuffer.WriteRune(letterRunes[binary.BigEndian.Uint32(Bytes(4))%letterLength])
+	}
+
+	return bytesBuffer.String()
+}
+
+func RandomInt(min int, max int) int {
+	return min + mathRand.Intn(max-min)
 }
