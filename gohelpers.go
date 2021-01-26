7205ef8c2f7f2570e4d05a5453bfd8a45aeccc47
MrAndreID <andreaadam215@gmail.com>
2021-01-26 10:24:42 +0700

    add handle response func. and handle json response func.;

--- gohelpers.go
+++ gohelpers.go
@@ -4,11 +4,52 @@ import (
 	"fmt"
 	"log"
 	"bytes"
+	"reflect"
+	"net/http"
 	"crypto/rand"
 	"encoding/json"
 	"encoding/base64"
 	"encoding/binary"
 )
+
+type Response struct {
+	Code	int			`json:"code"`
+	Status	string		`json:"status"`
+	Message string		`json:"message"`
+	Data	interface{}	`json:"data"`
+}
+
+type JSONResponse struct {
+	Status	string		`json:"status"`
+	Message string		`json:"message"`
+	Data	interface{}	`json:"data"`
+}
+
+func (response *Response) Success(code int, message string, data interface{}) {
+	response.Code = code
+	response.Status = "success"
+	response.Message = message
+	response.Data = data
+}
+
+func (response *Response) Error(code int, message string, data interface{}) {
+	response.Code = code
+	response.Status = "error"
+	response.Message = message
+	response.Data = data
+}
+
+func (response *JSONResponse) Success(message string, data interface{}) {
+	response.Status = "success"
+	response.Message = message
+	response.Data = data
+}
+
+func (response *JSONResponse) Error(message string, data interface{}) {
+	response.Status = "error"
+	response.Message = message
+	response.Data = data
+}
 
 func ErrorMessage(message string, err interface{}) {
 	fmt.Println()
@@ -21,6 +62,51 @@ func ErrorMessage(message string, err interface{}) {
 
 	log.Println("-------------------- End Of Error Message --------------------")
 	fmt.Println()
+}
+
+func HandleResponse(response http.ResponseWriter, code int, message string, data interface{}) {
+	var responseStruct = new(Response)
+
+	if code == 200 || code == 201 || code == 202 {
+		responseStruct.Success(code, message, data)
+	} else {
+		ErrorMessage(message, data)
+
+		if data == nil {
+			responseStruct.Error(code, message, nil)
+		} else if fmt.Sprintf("%v", reflect.TypeOf(data).Kind()) == "ptr" {
+			responseStruct.Error(code, message, fmt.Sprintf("%v", data))
+		} else {
+			responseStruct.Error(code, message, data)
+		}
+	}
+
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(code)
+	response.Write([]byte(JSONEncode(responseStruct)))
+}
+
+func HandleJSONResponse(status string, message string, data interface{}) string {
+	var responseStruct = new(JSONResponse)
+
+	if status == "success" {
+		responseStruct.Success(message, data)
+	} else {
+		ErrorMessage(message, data)
+
+		if data == nil {
+			responseStruct.Error(message, nil)
+		} else if fmt.Sprintf("%v", reflect.TypeOf(data).Kind()) == "ptr" {
+			responseStruct.Error(message, fmt.Sprintf("%v", data))
+		} else {
+			responseStruct.Error(message, data)
+		}
+	}
+
+	log.Println("Closing")
+	log.Println()
+
+	return JSONEncode(responseStruct)
 }
 
 func JSONEncode(data interface{}) string {
